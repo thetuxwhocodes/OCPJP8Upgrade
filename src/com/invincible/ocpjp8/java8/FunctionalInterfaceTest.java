e744ec2031c8bbfe81ec70b8b21fff1191577748
Girish B <thetuxwhocodes@users.noreply.github.com>
2016-11-04 21:24:46 +0530

    Upload

---
+++ src/com/invincible/ocpjp8/java8/FunctionalInterfaceTest.java
@@ -0,0 +1,20 @@
+package java8;
+
+public class FunctionalInterfaceTest {
+
+    public static void main(String[] args) {
+        Converter<Integer, Integer> RsToPaisaConverter = (from) -> from * 100;
+        Integer paisa = RsToPaisaConverter.convert(123);
+        System.out.println(paisa);    
+
+        Converter<Integer, Double> InchToCmConverter = (from) -> from * 2.5;
+        Double cms = InchToCmConverter.convert(123);
+        System.out.println(cms);    
+    }
+}
+
+@FunctionalInterface
+interface Converter<F, T> {
+
+    T convert(F from);
+}
